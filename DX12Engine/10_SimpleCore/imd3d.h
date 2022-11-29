a3a1f82218362c969c4746cc37395f9f18a6d36f
Lee Van <79038202+LeeIlJin@users.noreply.github.com>
2022-11-29 17:23:06 +0900

    Update imd3d.h

--- DX12Engine/10_SimpleCore/imd3d.h
+++ DX12Engine/10_SimpleCore/imd3d.h
@@ -1,5 +1,33 @@
 #pragma once
 
+/*
+檬扁拳 >>
+	颇捞橇扼牢
+	{
+		叼滚弊 拌摸 汲沥
+		Device 积己
+		Command Queue 积己
+		Sawp Chain 积己
+		RTV Descriptor Heap 积己
+		Command Allocator 积己
+	}
+
+	俊悸
+	{
+		Root Signature 积己
+		Shader 哪颇老
+		Vertex Input Layout 积己
+		Pipeline State 积己
+		Command List 积己
+		Command List 摧扁
+		Vertex Buffer 肺靛 棺 积己
+		Vertex Buffer View 积己
+		Fence 积己
+		Fence Event Handle 积己
+		Wait For GPU!
+	}
+*/
+
 namespace imd3d
 {
 	bool initialize_d3d();
